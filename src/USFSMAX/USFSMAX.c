0fb2fca6116de6daa23c6f19f1599fcf8c050b9b
Jake Carter <jake.carter@colorado.edu>
2020-09-22 16:41:57 -0600

    Re-introduce TMR-based delays

--- src/USFSMAX/USFSMAX.c
+++ src/USFSMAX/USFSMAX.c
@@ -1,17 +1,23 @@
-#include "USFSMAX.h"
-#include "i2c_helper.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "USFSMAX.h"
+#include "i2c_helper.h"
+#include "tmr_utils.h"
 
 // TODO : Implement all USFSMAX.h functions
+void delay(int ms) {
+	TMR_Delay(MXC_TMR0, MSEC(ms), 0);
+}
 
 void USFSMAX_init() {
 	printf("Initializing USFSMAX Coprocessor...\n");
 	uint8_t status = i2c_readByte(USFSMAX_ADDR, FUSION_STATUS);
+	delay(100);
 
 	if (status == 0) { // No errors/alarms
 		i2c_writeByte(USFSMAX_ADDR, FUSION_START_STOP, 0x0); // Stop the sensor fusion.  Configuration can only be written if fusion loop is stopped.
+		delay(100);
 
 		CoProcessorConfig_t config; // Set up config struct from values set in config.h (I don't really like the way this is done but it's how it's done in the original source.  TODO : Do this better
 		config.cal_points        = CAL_POINTS;
@@ -56,21 +62,33 @@ void USFSMAX_init() {
 
 		printf("Writing configuration to device...");
 		i2c_writeBytes(USFSMAX_ADDR, COPRO_CFG_DATA0, 30, unpacked);
+		delay(100);
 		i2c_writeBytes(USFSMAX_ADDR, COPRO_CFG_DATA1, sizeof(CoProcessorConfig_t) - 30, &unpacked[30]);
+		delay(100);
 		printf("Done!\n");
 
 		// Restart sensor fusion
 		printf("Restarting sensor fusion...");
 		i2c_writeByte(USFSMAX_ADDR, FUSION_START_STOP, 0x1);
+		delay(100);
 
-		// Poll FUSION_STATUS register to see if fusion has resumed
+		// Poll FUSION_STATUS register to see if fusion has resumed, with a timeout of 2s
 		printf("Verifying...");
+
+		int count = 0;
 		status = i2c_readByte(USFSMAX_ADDR, FUSION_STATUS);
+		while (!(status & FUSION_RUNNING_MASK) && count < 2000) {
+			delay(10);
+			count += 10;
+			status = i2c_readByte(USFSMAX_ADDR, FUSION_STATUS);
+		}
+
 		if (status == 0) { printf("Fusion loop started!\n"); }
 		else { printf("Failed to start fusion loop!\n"); return; }
 
 		// Check for sensor errors
 		status = i2c_readByte(USFSMAX_ADDR, SENS_ERR_STAT);
+		delay(100);
 		if (status != 0) {
 			printf("Sensor error!  Status : %i\n", status);
 			// TODO : Deal with sensor errors.
@@ -83,9 +101,11 @@ void USFSMAX_init() {
 		    if(USE_2D_DHI_CORRECTOR)
 		    {
 		      i2c_writeByte(MAX32660_SLV_ADDR, CALIBRATION_REQUEST, 0x50); // Enable DHI corrector, 2D (0x10|0x50)
+		      delay(100);
 		    } else
 		    {
 		      i2c_writeByte(MAX32660_SLV_ADDR, CALIBRATION_REQUEST, 0x10); // Enable DHI corrector, 3D (0x10)
+		      delay(100);
 		    }
 		  }
 
